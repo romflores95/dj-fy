14ef3d0b2c6a4814eb50ba003ced038747889765
Romeo Flores <roflores@mit.edu>
2018-02-17 17:01:52 -0500

    added apis on app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -16,7 +20,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'tapRootIcon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
